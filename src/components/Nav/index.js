c3cf10da89b372a0463b3c59b0e6faeedac82b1d
Mystic Kitty <djabranton@gmail.com>
2021-02-18 11:12:51 -0800

    Create fragment on nav page to enclose Dani link and icon.  Moved over from Header.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import coverImage from '../../assets/img/pixels4.png';
 
-function Nav() {
 
-    return (
-        <nav>
-            <ul className="flex-row">
-                <li>About</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-                <li>Resume</li>
-            </ul>
-        </nav>
-    );
+class Nav extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="head-container">
+                    <h1 class="right">
+                        <a href="/">Dani codes</a><img src={coverImage} style={{ height: "30px", width: "30px" }} alt="pixels" />
+
+                    </h1>
+                    <nav>
+                        <ul className="flex-row">
+                            <li>About</li>
+                            <li>Portfolio</li>
+                            <li>Contact</li>
+                            <li>Resume</li>
+                        </ul>
+                    </nav></div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Nav;
