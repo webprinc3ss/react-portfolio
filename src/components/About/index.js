73e7ec2bb27f92f236b3cfc238741874a4315e8f
Mystic Kitty <djabranton@gmail.com>
2021-02-21 14:16:57 -0800

    Created grid for resume page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
             <h2>Welcome</h2><br></br>
             <p className="center"><img className="dani-image" src={daniImage} alt="Me" /></p>
             <div className="about-container"><p><br></br>
-            My background is web design and website administration. My strengths include matching a technology with a need and the ability to integrate the solution. Now I am happy to learn develop with these technologies and am happy to share my latest projects I've created on my journey to full stack web developer!
+            My background is web design and website administration. My strengths include matching a technology with a need and the ability to integrate the solution. Now I can develop with these technologies and am happy to share my latest projects on my journey to full stack web developer!
             </p></div>
         </section>
     )
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,11 +6,37 @@ function Resume() {
         <section>
 
             <h2>Resume</h2>
+            <h4><a href="https://docs.google.com/document/d/1GtRqjSDouVMHkc4ZLZ9N_Uk4OES5IyDuAwxkwwyCJPs/edit?usp=sharing" target="_new">Download</a></h4>
+            <div class="grid-container">
+                <div class="grid-item">
 
-            <p><a href="#">View</a>
-            </p>
+                    <h5>Front-End Proficiencies</h5>
+                    <p className="skills">HTML</p>
+                    <p className="skills">CSS</p>
+                    <p className="skills">JavaScript</p>
+                    <p className="skills">JQuery</p>
+                    <p className="skills">Responsive Design</p>
+                    <p className="skills">React</p>
+                    <p className="skills">Bootstrap</p>
 
-            <img src={laptop} alt="laptop" className="laptop" />
+
+                </div>
+                <div class="grid-item">
+
+                    <h5>Back-End Proficiencies</h5>
+                    <p className="skills">API's</p>
+                    <p className="skills">Node.js</p>
+                    <p className="skills">Express, Express-Session</p>
+                    <p className="skills">My Sequel, Sequelize</p>
+                    <p className="skills">MongoDB, Mongoose</p>
+                    <p className="skills">Handlebars</p>
+                    {/* <p className="skills">Bootstrap</p> */}
+
+                </div>
+
+            </div>
+
+
 
         </section>
     )
