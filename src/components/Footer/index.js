8cca644154404eb08cc070da5954b90c2be82e04
Mystic Kitty <djabranton@gmail.com>
2021-03-08 14:25:58 -0800

    Adjusting link styling.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,9 +6,9 @@ function Footer() {
         <footer>
             <p>Thank you for visiting!</p>
             <p>
-                <a href="https://github.com/webprinc3ss"><i class="fab fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/danielle-branton-0051951/"><i class="fab fa-linkedin"></i></a>
-                <a href="https://twitter.com/webprincess"><i class="fab fa-twitter"></i></a>
+                <a href="https://github.com/webprinc3ss"><i class="fab fa-github icon"></i></a>
+                <a href="https://www.linkedin.com/in/danielle-branton-0051951/"><i class="fab fa-linkedin icon"></i></a>
+                <a href="https://twitter.com/webprincess"><i class="fab fa-twitter icon"></i></a>
 
             </p>
             <span className="copyends">
