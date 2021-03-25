028680f1f550c1c81f4e2eb5f924d406ed7e204d
Mystic Kitty <djabranton@gmail.com>
2021-03-25 13:11:13 -0700

    Updated Resume page.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -16,18 +16,41 @@ function Resume() {
                     <p className="skills">Responsive Design</p>
                     <p className="skills">React</p>
                     <p className="skills">Bootstrap</p>
-
-
+                    <p className="skills">Semantic Ui React</p>
+                    <p className="skills">Cloudinary</p>
+                    <p className="skills">Word Press Customization</p>
                 </div>
                 <div class="grid-item">
                     <h5>Back-End Proficiencies</h5>
-                    <p className="skills">API's</p>
+                    <p className="skills">Server-side API's</p>
                     <p className="skills">Node.js</p>
                     <p className="skills">Express, Express-Session</p>
+                    <p className="skills">RESTful API's</p>
                     <p className="skills">GraphQL</p>
                     <p className="skills">My Sequel, Sequelize</p>
                     <p className="skills">MongoDB, Mongoose</p>
                     <p className="skills">Handlebars</p>
+                    <p className="skills">Apollo Server</p>
+                    <p className="skills">Multiparty</p>
+                    <p className="skills">PWA's</p>
+                </div>
+                <div class="grid-item">
+                    <h5>Web Administration</h5>
+                    <p className="skills">Google Ads</p>
+                    <p className="skills">Drupal CMS</p>
+                    <p className="skills">Google Adwords</p>
+                    <p className="skills">Neon CMS</p>
+                    <p className="skills">Help Desk</p>
+                    <p className="skills">Training and Support</p>
+                    <p className="skills">Bootstrap</p>
+                </div>
+                <div class="grid-item">
+                    <h5>Other Skills</h5>
+                    <p className="skills">Adobe PhotoShop</p>
+                    <p className="skills">Adobe Illustrator</p>
+                    <p className="skills">Presentation and Meeting Facilitation</p>
+                    <p className="skills">Customer Service Phones</p>
+                    <p className="skills">Event Planning</p>
                 </div>
             </div>
         </section>
