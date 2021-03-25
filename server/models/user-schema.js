3451fb8d980e0407fdc4bdc3ada349f769301750
Mystic Kitty <djabranton@gmail.com>
2021-03-25 15:49:51 -0700

    Creating models and schema.

---
+++ server/models/user-schema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    subject: {
+        type: String
+    },
+    message: {
+        type: String
+    }
+}, {
+    collection: 'users'
+})
+
+module.exports = mongoose.model('User', userSchema)
